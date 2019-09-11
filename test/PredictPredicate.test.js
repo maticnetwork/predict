3a63f195d84e7a239f4cc6d5efc052632824b781
atvanguard <93arpit@gmail.com>
2019-09-11 10:36:39 +0530

    give rep and cash

--- test/PredictPredicate.test.js
+++ test/PredictPredicate.test.js
@@ -1,6 +1,7 @@
 // const PredictPredicate = artifacts.require('PredictPredicate')
 const augurHelper = require('./helpers/AugurHelper.js')
 const abis = require('./helpers/AugurContracts/abi.json')
+let addresses = require('./helpers/AugurContracts/addresses2.json')
 
 contract('PredictPredicate', async function(accounts) {
   let predictPredicate, from = accounts[0]
@@ -11,6 +12,13 @@ contract('PredictPredicate', async function(accounts) {
 
   it.only('createYesNoMarket', async function() {
     const universe = augurHelper.getUniverse()
+    const repAddress = await universe.methods.getReputationToken().call({ from: accounts[0] })
+    const repTransfer = await (new web3.eth.Contract(abis.TestNetReputationToken, repAddress)).methods.faucet(0).send({ from })
+    // '0x' + web3.utils.toBN(10).pow(web3.utils.toBN(18)).toString(16)
+    const amount = await universe.methods.getOrCacheValidityBond().call()
+    const cash = (new web3.eth.Contract(abis.Cash, addresses.Cash)).methods
+    await cash.faucet(amount).send({ from })
+    await cash.approve(addresses.Augur, amount).send({ from })
     const createMarket = await universe.methods.createYesNoMarket(1568197952, 0, 0, accounts[0], '').send({ from })
     console.log(createMarket)
   })
--- test/helpers/AugurHelper.js
+++ test/helpers/AugurHelper.js
@@ -1,7 +1,7 @@
 const abis = require('./AugurContracts/abi.json')
-// let addresses = require('./AugurContracts/addresses2.json')
-let addresses = require('./AugurContracts/addresses.json')
-addresses = addresses['103']
+let addresses = require('./AugurContracts/addresses2.json')
+// let addresses = require('./AugurContracts/addresses.json')
+// addresses = addresses['103']
 
 class AugurHelper {
   getAugur() {
@@ -9,7 +9,6 @@ class AugurHelper {
   }
 
   getUniverse() {
-    console.log(addresses.Universe)
     return new web3.eth.Contract(abis.Universe, addresses.Universe)
   }
 }
