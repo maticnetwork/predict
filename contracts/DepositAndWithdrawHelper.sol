79903f604e2d3ac2453760f836faeceedb26a362
atvanguard <93arpit@gmail.com>
2019-09-12 18:22:49 +0530

    basic outline for startExitForYesNoMarketShares

--- contracts/DepositAndWithdrawHelper.sol
+++ contracts/DepositAndWithdrawHelper.sol
@@ -1,14 +1,25 @@
 pragma solidity 0.5.10;
 
+import { Registry } from "./Registry.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { DepositManager } from "matic-protocol/contracts/root/depositManager/DepositManager.sol";
 
 import { IOICash } from "./predict/trading/IOICash.sol";
 
-contract DepositHelper {
+contract DepositAndWithdrawHelper {
+  Registry public registry;
   IERC20 public dai;
   DepositManager public depositManager;
   IOICash public oICash;
+
+  struct Exit {
+    address owner;
+    address market;
+    uint256[] balances; // indexed by outcome id
+  }
+
+  mapping(uint256 => Exit) public exits;
+  uint256 public exitId;
 
   constructor(address _dai, address _oICash, address payable _depositManager) public {
     dai = IERC20(_dai);
@@ -26,5 +37,14 @@ contract DepositHelper {
       "OICash_DEPOSIT_FAILED"
     );
     depositManager.depositERC20ForUser(address(dai), msg.sender, amount);
+  }
+
+  function process(address owner, address market, uint256[] calldata balances)
+    external
+    /* onlyPredicate */
+  {
+    address _market = registry.childToRootMarket(market);
+    // market interaction to buy shares using OICash
+    exits[exitId++] = Exit(owner, _market, balances);
   }
 }
